0f9c63a0adb0410d54825b017b710a6ca4d0713c
Lcky <kiper26106282@gmail.com>
2026-08-16 22:28:02 +0300

    fix: Restore top 'Active Server' button and bottom 'Settings' button in Main Menu

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -6,7 +6,7 @@ def main_menu_kb(has_creds: bool = True, active_panel_name: str = "Main Server",
     buttons = []
     if has_creds:
         buttons.append([
-            InlineKeyboardButton(text=t("btn_settings", lang), callback_data="menu_bot_dashboard")
+            InlineKeyboardButton(text=t("btn_active_server", lang, name=active_panel_name), callback_data="menu_select_panel")
         ])
         buttons.append([
             InlineKeyboardButton(text=t("btn_server_status", lang), callback_data="menu_server")
@@ -25,6 +25,9 @@ def main_menu_kb(has_creds: bool = True, active_panel_name: str = "Main Server",
         ])
         buttons.append([
             InlineKeyboardButton(text=t("btn_restart_xray", lang), callback_data="action_restart_xray")
+        ])
+        buttons.append([
+            InlineKeyboardButton(text=t("btn_settings", lang), callback_data="menu_settings")
         ])
     else:
         buttons.append([
